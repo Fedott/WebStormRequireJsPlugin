f21948a1754d7486d378b8a157d66be3751be51b
Fedot <fedotru@gmail.com>
2014-01-08 21:39:14 +0400

    Fix issue #9 Commit lost test file

---
+++ testData/public/fileForTestConfigWithRelativePathReference.js
@@ -0,0 +1,8 @@
+define(function(require) {
+    var depend1 = require('pathForBlock');
+    var depend2 = require('pathForDirectoryTwoDot/block');
+    var depend3 = require('pathForDirectoryOneDot/kits/kit');
+    var depend4 = require('pathForKit');
+    var depend5 = require('pathForDirOneDotWithDir/kit');
+    var depend6 = require('pathForNotFound')
+})
