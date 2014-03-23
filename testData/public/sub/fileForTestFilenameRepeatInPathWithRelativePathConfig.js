a4b83ef88423423c7dfc3f8bac29ab39b0b3b5af
Fedot <fedotru@gmail.com>
2014-03-23 17:20:44 +0400

    Fixed #21

---
+++ testData/public/sub/fileForTestFilenameRepeatInPathWithRelativePathConfig.js
@@ -0,0 +1,10 @@
+require.config({
+    baseUrl: '/',
+    paths: {
+        'mout': '../vendor/mout/src',
+
+
+    }
+});
+
+var reference = require('mout/lang/file');
---
+++ tests/requirejs/FilenameRepeatInPathTest.java
@@ -0,0 +1,36 @@
+package requirejs;
+
+import com.intellij.psi.PsiReference;
+import requirejs.settings.Settings;
+
+public class FilenameRepeatInPathTest extends RequirejsTestCase {
+    @Override
+    public void setUp() throws Exception {
+        super.setUp();
+        myFixture.configureByFiles(
+                "public/fileForTestFilenameRepeatInPath.js",
+                "public/vendor/mout/src/lang/file.js"
+        );
+        setWebPathSetting();
+
+        Settings.getInstance(getProject()).configFilePath = "fileForTestFilenameRepeatInPath.js";
+    }
+
+    public void testCompletion() {
+
+    }
+
+    public void testReference() {
+        myFixture.configureByFile("public/fileForTestFilenameRepeatInPath.js");
+
+        PsiReference reference = getReferenceForHumanPosition(10, 33);
+        assertReference(reference, "mout/lang/file", "file.js");
+    }
+
+    public void testReferenceWithRelativePathConfig() {
+        myFixture.configureByFile("public/sub/fileForTestFilenameRepeatInPathWithRelativePathConfig.js");
+
+        PsiReference reference = getReferenceForHumanPosition(10, 33);
+        assertReference(reference, "mout/lang/file", "file.js");
+    }
+}
